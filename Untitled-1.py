7bdc922ac2d1b7c495cadfde2642e039cb643183
Yasin <Ismaielyasin@gmail.com>
2024-03-29 17:21:48 +0000

    comments have been added

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,15 +1,17 @@
-rent = int(input("enter rent per month:"))
+# this program calculates your total expenditure for the month and the amount remamining.
+
+rent = int(input("enter rent per month:")) 
 
 loan = int(input("enter loan repayment per month:"))
 
 childcare = int(input("enter childcare per month:"))
 
-total_outgoings = rent + loan + childcare
+total_outgoings = rent + loan + childcare # calculates the outgoings
 
-print (f"the total outgoings for the month is £{total_outgoings}")
+print (f"the total outgoings for the month is £{total_outgoings}") # prints the total expenditure
 
 monthly_wage = int(input("enter wage per month:"))
 
 amount_remaining = monthly_wage - total_outgoings
 
-print(f"the amount remaining for the month is £{amount_remaining}")
+print(f"the amount remaining for the month is £{amount_remaining}") # prints the amount remaining
